e316864803d8bd911202beb398ff5d520cd7ed3a
Markus Uusitalo <markusuusitalo@Markuss-MacBook-Air.local>
2023-07-13 16:29:19 +0300

    first commit

---
+++ services/chatServices.js
@@ -0,0 +1,14 @@
+import postgres from "https://deno.land/x/postgresjs@v3.3.3/mod.js";
+
+const sql = postgres ({});
+
+const fiveMessages = async () => {
+    const rows = await sql`SELECT * FROM messages ORDER BY id DESC LIMIT 5`;
+    return rows;
+}
+
+const create = async (sender, message) => {
+    await sql`INSERT INTO messages (sender, message) VALUES (${sender}, message ${message})`;
+}
+
+export {fiveMessages, create}
